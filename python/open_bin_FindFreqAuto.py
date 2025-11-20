6736c5821afbab2df54a79824667a5f18d7a1d53
page-icube <33966313+klaw83@users.noreply.github.com>
2025-11-20 10:44:38 +0100

    Everythign in one loop

--- python/open_bin_FindFreqAuto.py
+++ python/open_bin_FindFreqAuto.py
@@ -27,10 +27,10 @@ file_path_all = filedialog.askopenfilename()
 FidNb = -1 #-1 Pour prendre toutes les FID
 
 ######### EXTRACTION OF PARAMETERS FROM FILENAME #############
-Start_freq = float(file_path_all.split('_')[3]) - 5000
+Start_freq = 0 #float(file_path_all.split('_')[3]) - 5000
 Step_freq = float(file_path_all.split('_')[2])
-Number_of_files = int(file_path_all.split('_')[1])
-graphstart = 0.2 # in ms
+Number_of_files = 350 #int(file_path_all.split('_')[1])
+graphstart = 0 # in ms
 graphstop = 0.1 # in ms
 
 print("Ouverture de : "+file_path_all)
@@ -42,13 +42,44 @@ progress_bar = tqdm(total=Number_of_files-1, desc="Processing files FID", unit="
 for i in range(Number_of_files):
     progress_bar.update(1)
     file_path = file_path_all + str(i)
+
+    # Opening the binary file
     time_array, voltage_array_matrix, voltageAcc_array = nmr.open_file_bin(file_path, nombre_de_FID=FidNb)
 
+    #Filtrage :
+    fs = 1/((time_array[10]-time_array[0])/10) 
+    lowcut = 1000000.0
+    highcut = 20000000
+    voltageAcc_array_filtered = nmr.butter_bandpass_filter(voltageAcc_array, lowcut, highcut, fs, order=1)
+
+    ## Removing the edges of the FID according to graphstart and graphstop
     dt = np.abs(time_array[0] - time_array[1])   
-    voltageAcc_array = voltageAcc_array[int(graphstart/(1000*dt)):len(voltageAcc_array)-int(graphstop/(1000*dt))]
+    voltageAcc_array = voltageAcc_array_filtered[int(graphstart/(1000*dt)):len(voltageAcc_array)-int(graphstop/(1000*dt))]
     time_array = time_array[int(graphstart/(1000*dt)):len(voltageAcc_array)+int(graphstart/(1000*dt))]
     
-    # Affichage du signal accumulé
+    # omputing the excitation frequency for this acquisition
+    freq_ex = Start_freq + Step_freq*i 
+
+    ## Calcul de la TF
+    dt = np.abs(time_array[0] - time_array[1])   
+    N = len(voltageAcc_array)
+    freq = np.fft.fftfreq(N, dt)
+    fft_values = np.fft.fft(voltageAcc_array)
+    freq = freq + freq_ex
+    magnitude = np.abs(fft_values) * 2 / N  # Normalize amplitude
+
+    # Summing all TFs together
+    if i==0:
+        freq_all = freq
+        tf_sum = magnitude
+    g0 = interp1d(freq_all, tf_sum,bounds_error=False,fill_value=0.0)
+    freq_all = np.union1d(freq_all, freq)
+    g1 = interp1d(freq, magnitude,bounds_error=False,fill_value=0.0)
+    g1_values = g1(freq_all)
+    g0_values = g0(freq_all)
+    tf_sum = g1_values + g0_values 
+
+    # Plotting the FID (time domain)
     plt.figure(1)
     plt.plot(time_array, voltageAcc_array, marker='+', linestyle='-', label=str(i), linewidth=2)
     plt.title(f'{graph_name} - Accumulation de {FidNb}')
@@ -57,67 +88,26 @@ for i in range(Number_of_files):
     plt.grid(True, which='both', linestyle='--', linewidth=0.5, alpha=0.7)
     plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), ncol=2)
 
-### plot FT
-progress_bar.close()
-progress_bar = tqdm(total=Number_of_files-1, desc="Processing files TF", unit="file")
-
-for i in range(Number_of_files):
-    progress_bar.update(1)
-    file_path = file_path_all + str(i)
-    time_array, voltage_array_matrix, voltageAcc_array = nmr.open_file_bin(file_path, nombre_de_FID=FidNb)
-    
-
-    freq_ex = Start_freq + Step_freq*i
-    
-
-    #Filtrage :
-    fs = 1/((time_array[10]-time_array[0])/10) 
-    lowcut = 100.0
-    highcut = 2000.0
-    voltageAcc_array_filtered = nmr.butter_bandpass_filter(voltageAcc_array, lowcut, highcut, fs, order=3)
-    
-    ## Calcul de la TF
-    
-    dt = np.abs(time_array[0] - time_array[1])   
-    voltageAcc_array = voltageAcc_array[int(graphstart/(1000*dt)):len(voltageAcc_array)-int(graphstop/(1000*dt))]
-
-    N = len(voltageAcc_array)
-    freq = np.fft.fftfreq(N, dt)
-
-    fft_values = np.fft.fft(voltageAcc_array)
-    freq = freq + freq_ex
-    magnitude = np.abs(fft_values) * 2 / N  # Normalize amplitude
-
-    if i==0:
-        freq_all = freq
-        tf_sum = magnitude
-
-    g0 = interp1d(freq_all, tf_sum,bounds_error=False,fill_value=0.0)
-    freq_all = np.union1d(freq_all, freq)
-    g1 = interp1d(freq, magnitude,bounds_error=False,fill_value=0.0)
-    g1_values = g1(freq_all)
-    g0_values = g0(freq_all)
-
-    tf_sum = g1_values + g0_values 
-
+    # Plotting the TF
     plt.figure(2)
     plt.plot(freq, magnitude, label= str(i),marker='x', linestyle='-')
     plt.title("Fourier Transform - " + graph_name)
     plt.xlabel("Frequency [Hz]")
     plt.ylabel("Amplitude")
     plt.legend(loc='center left', bbox_to_anchor=(1, 0.5), ncol=2)
-    #plt.tight_layout(rect=[0, 0, 0.85, 1])
     plt.grid(True, which='both')
     plt.minorticks_on()
     plt.grid(which='minor', alpha=0.2)
     plt.grid(which='major', alpha=0.5)
 
 progress_bar.close()
-plt.figure()
+
 max_tf = np.max(tf_sum)
 max_freq = freq_all[np.argmax(tf_sum)]
 print(f"\033[92m Larmor Frequency : {max_freq} Hz\033[0m")
 
+#plotting the summed TF
+plt.figure(3)
 plt.legend(['Sum TF', f"Max: {max_tf:.2f} at {max_freq:.2f} Hz"], loc='center left', bbox_to_anchor=(1, 0.5))
 plt.plot(freq_all, tf_sum, label='Sum TF', marker='x', linestyle='-')
 plt.title("Sum Fourier Transform to find freq- " + graph_name)
